1cfa2a4344ed6d689f24b97258513bbe377aac4d
Sefa <sefa_guercan@hotmail.de>
2026-07-06 23:46:40 +0200

    update files for login, board and summary

---
+++ js/board.js
@@ -0,0 +1,36 @@
+// variables //
+const taskOverlay = document.getElementById('task-overlay');
+const editOverlay = document.getElementById('edit-overlay');
+
+// functions //
+
+
+// es wird der login checkup benötigt //
+function init() {
+
+}
+
+function getToAddTask() {
+    window.location.href = 'add_task.html';
+}
+
+// Task Overlay //
+
+function openTaskOverlay() {
+    taskOverlay.showModal();
+}
+
+function closeTaskOverlay() {
+    taskOverlay.close();
+}
+
+// Edit Overlay //
+
+function getEditOverlay() {
+    taskOverlay.close();
+    editOverlay.showModal();
+}
+
+function closeEditOverlay() {
+    editOverlay.close();
+}

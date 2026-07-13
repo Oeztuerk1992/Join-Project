32c65ce8ba281b9e885aa72ba6deeebe6884dfa1
Sefa <sefa_guercan@hotmail.de>
2026-07-13 18:43:24 +0200

    update main layout

--- js/declarations.js
+++ js/declarations.js
@@ -1,3 +1,65 @@
+const rightHeader = document.getElementById('right-header');
+const leftSideBar = document.getElementById('nav-sidebar');
+const sideBarGuest = document.getElementById('sidebar-guest');
+const helpBtn = document.getElementById('help-btn');
+
+const currentPage = window.location.pathname;
+
+
+function initDeclarations() {
+    
+    checkForHeader();
+    checkForSidebar();
+    checkForHelpBtn();
+
+    getUserProfile();
+}
+
+function checkForHeader() {
+
+    const isInfoPage =
+        currentPage.includes('legal_notice.html') ||
+        currentPage.includes('privacy_policy.html');
+
+    if (isInfoPage && !loggedInUser) {
+        rightHeader.classList.add('hidden');
+    }
+    if (isInfoPage && !loggedInUser) {
+        rightHeader.classList.add('hidden');
+    }
+}
+
+function checkForSidebar() {
+
+    if (!loggedInUser) {
+        leftSideBar.classList.add('hidden');
+        sideBarGuest.classList.remove('hidden');
+    } else {
+
+        leftSideBar.classList.remove('hidden');
+        sideBarGuest.classList.add('hidden');
+    }
+}
+
+function checkForHelpBtn() {
+    helpBtn.style.display = 'none';
+}
+
+function getUserProfile() {
+
+    if (loggedInUser === null) {
+        return;
+    }
+
+    if (loggedInUser === 'guest') {
+        userProfile.textContent = "G";
+    } else {
+        const name = loggedInUser.split(" ");
+        const initials = name[0][0].toUpperCase() + name[1][0].toUpperCase();
+        userProfile.textContent = initials;
+    }
+}
+
 // functions for help-html //
 
 function goBack() {
--- js/desktop_template.js
+++ js/desktop_template.js
@@ -1,15 +1,22 @@
 // Variables //
 
 const userMenu = document.getElementById("user-nav");
+const circle = document.querySelector('.circle');
+
+const loggedInUser = sessionStorage.getItem('loggedInUser');
+const userProfile = document.getElementById('initials-user');
 
 // functions //
 
 function toggleUserMenu() {
+    
     if (userMenu.open) {
         userMenu.close();
     } else {
         userMenu.show();
     }
+
+    circle.classList.toggle('active', userMenu.open);
 }
 
 // Event Listeners //
@@ -21,5 +28,10 @@ document.addEventListener("click", (event) => {
         !event.target.closest(".user-menu")
     ) {
         userMenu.close();
+        circle.classList.toggle('active', userMenu.open);
     }
 });
+
+function logout() {
+    sessionStorage.clear();
+}
